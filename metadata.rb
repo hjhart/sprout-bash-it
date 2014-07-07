9b4f607ec111c9d4751a0ba6f09943696fdaa4b6
James Hart <james@wanelo.com>
2014-07-07 01:21:01 -0700

    Move attributes, recipes, resources, providers from old recipe to new cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
-name             'sprout-exemplar'
-maintainer       'Pivotal'
-maintainer_email 'sprout-maintainers@pivotallabs.com'
+name             'sprout-bash-it'
+maintainer       'James Hart'
+maintainer_email 'hjhart@gmail.com'
 license          'MIT'
-description      'Template repo for creating standalone cookbooks'
+description      'Cookbook to manage bash-it on OSX workstations'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 supports         'mac_os_x'
