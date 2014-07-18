f22b79421075003d33735e08c4128ea0957f198f
James Hart <james@wanelo.com>
2014-07-18 15:09:40 -0700

    Backfill some specs

--- spec/unit/default_spec.rb
+++ spec/unit/default_spec.rb
@@ -1,10 +1,22 @@
 require 'unit/spec_helper'
 
-describe 'sprout-exemplar::default' do
+describe 'sprout-bash-it::default' do
   let(:runner) { ChefSpec::Runner.new }
 
-  it 'includes the path recipe' do
+  it 'clones the git repository into the chef cache directory' do
     runner.converge(described_recipe)
-    expect(runner).to include_recipe('sprout-exemplar::path')
+    expect(runner).to sync_git("#{Chef::Config[:file_cache_path]}/bash_it")
+  end
+  context 'when the directory is specified' do
+    let(:runner) {
+      ChefSpec::Runner.new do |node|
+        node.set['bash_it']['dir'] = '/Users/hjhart/.bash_it'
+      end
+    }
+
+    it 'creates the bash_it directory, as specified' do
+      runner.converge(described_recipe)
+      expect(runner).to create_directory('/Users/hjhart/.bash_it')
+    end
   end
 end
