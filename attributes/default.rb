498d1a6b0195fed356464d5d5e16d69e93d800f5
James Hart <james@wanelo.com>
2014-07-18 14:57:16 -0700

    Appease the RuboCop gods

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,21 +1,21 @@
-include_attribute "sprout-base::home"
+include_attribute 'sprout-base::home'
 
 node.default['bash_it']['enabled_plugins'] = {
-  'aliases' => %w[general],
-  'completion' => %w[defaults git],
-  'plugins' => %w[ssh]
+  'aliases' => %w(general),
+  'completion' => %w(defaults git),
+  'plugins' => %w(ssh)
 }
 
 node.default['bash_it']['custom_plugins'] = {
-  "sprout-base" => %w[
+  'sprout-base' => %w(
     bash_it/custom/disable_ctrl-s_output_control.bash
     bash_it/custom/enable_ctrl-o_history_execution.bash
     bash_it/custom/ensure_usr_local_bin_first.bash
     bash_it/custom/add_user_initials_to_git_prompt_info.bash
-  ]
+  )
 }
 
 node.default['bash_it']['theme'] = 'bobby'
-node.default['bash_it']['dir'] = ::File.expand_path(".bash_it", node['sprout']['home'])
-node.default['bash_it']['bashrc_path'] = ::File.expand_path(".bash_profile", node['sprout']['home'])
+node.default['bash_it']['dir'] = ::File.expand_path('.bash_it', node['sprout']['home'])
+node.default['bash_it']['bashrc_path'] = ::File.expand_path('.bash_profile', node['sprout']['home'])
 node.default['bash_it']['repository'] = 'http://github.com/revans/bash-it.git'
