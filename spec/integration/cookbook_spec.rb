b77705dec8abd5556097affb6d1efd3630447dbd
James Hart <james@wanelo.com>
2014-07-18 14:30:38 -0700

    Expect directory to be created, expect .bash_profile to be created

--- spec/integration/cookbook_spec.rb
+++ spec/integration/cookbook_spec.rb
@@ -1,16 +1,16 @@
 require 'spec_helper'
 
-describe 'sprout-exemplar' do
+describe 'sprout-bash-it::default' do
   before :all do
-    expect(File).not_to be_exists("#{ENV['HOME']}/exemplar")
+    expect(File).not_to be_exists("#{ENV['HOME']}/.bash_it")
     expect(system('soloist')).to eq(true)
   end
 
-  it 'creates a file in the home directory' do
-    expect(File).to be_exists("#{ENV['HOME']}/exemplar")
+  it 'creates the .bash_it directory in the home directory' do
+    expect(File).to be_exists("#{ENV['HOME']}/.bash_it")
   end
 
-  it 'is owned by the current user not root' do
-    expect(File.stat("#{ENV['HOME']}/exemplar")).to be_owned
+  it 'creates a .bash_profile' do
+    expect(File).to be_exists("#{ENV['HOME']}/.bash_profile")
   end
 end
