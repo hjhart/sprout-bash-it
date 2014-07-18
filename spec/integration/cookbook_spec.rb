63ef4a14864e27b7beead7285ded8140a74b4a03
James Hart <james@wanelo.com>
2014-07-18 16:39:44 -0700

    Test LWRP through integration spec.  - Apparently it is difficult (or impossible?) to test LWRP w/ ChefSpec :sob:

--- spec/integration/cookbook_spec.rb
+++ spec/integration/cookbook_spec.rb
@@ -13,4 +13,20 @@ describe 'sprout-bash-it::default' do
   it 'creates a .bash_profile' do
     expect(File).to be_exists("#{ENV['HOME']}/.bash_profile")
   end
+
+  it 'creates custom plugins' do
+    expect(File).to be_exists("#{ENV['HOME']}/.bash_it/custom/disable_ctrl-s_output_control.bash")
+  end
+
+  it 'creates enabled aliases' do
+    expect(File).to be_exists("#{ENV['HOME']}/.bash_it/aliases/enabled/general.aliases.bash")
+  end
+
+  it 'creates enabled completions' do
+    expect(File).to be_exists("#{ENV['HOME']}/.bash_it/completion/enabled/git.completion.bash")
+  end
+
+  it 'creates enabled plugins' do
+    expect(File).to be_exists("#{ENV['HOME']}/.bash_it/plugins/enabled/ssh.plugin.bash")
+  end
 end
